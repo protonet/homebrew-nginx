525750ddb8b72eeb2727efd17487e4bf3ae38db4
Frederik Dietz <fdietz@gmail.com>
2013-12-10 11:25:33 +0100

    most recent version

--- upload-nginx-module.rb
+++ upload-nginx-module.rb
@@ -3,7 +3,7 @@ require 'formula'
 class UploadNginxModule < Formula
 
   homepage 'http://wiki.nginx.org/HttpUploadModule'
-  url 'http://www.grid.net.ru/nginx/download/nginx_upload_module-2.2.0.tar.gz'
+  url 'https://github.com/vkholodkov/nginx-upload-module/archive/2.2.tar.gz'
   sha1 '93d6e83e613a0ce2ed057a434b344fa1b6609b47'
 
   def install
