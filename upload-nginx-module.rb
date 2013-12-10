4a8fbf6469ef647e30f2e222439a057fa4e1545c
Frederik Dietz <fdietz@gmail.com>
2013-12-10 11:27:21 +0100

    most recent version

--- upload-nginx-module.rb
+++ upload-nginx-module.rb
@@ -4,7 +4,7 @@ class UploadNginxModule < Formula
 
   homepage 'http://wiki.nginx.org/HttpUploadModule'
   url 'https://github.com/vkholodkov/nginx-upload-module/archive/2.2.tar.gz'
-  sha1 '93d6e83e613a0ce2ed057a434b344fa1b6609b47'
+  sha1 'c8cbeddce12e80e4103e6a3a86754a331bd7ab8a'
 
   def install
     (share+'upload-nginx-module').install Dir['*']
