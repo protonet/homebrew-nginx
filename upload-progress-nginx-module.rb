a4599641e63a89529021f5070dad9e4784a089d4
Frederik Dietz <fdietz@gmail.com>
2013-12-10 11:41:47 +0100

    add version info

--- upload-progress-nginx-module.rb
+++ upload-progress-nginx-module.rb
@@ -5,6 +5,7 @@ class UploadProgressNginxModule < Formula
   homepage 'http://wiki.nginx.org/NginxHttpUploadProgressModule'
   url 'https://github.com/masterzen/nginx-upload-progress-module/archive/master.tar.gz'
   sha1 '3400b9c7ee7ec1dfa0ee3e3053a6329d0236550c'
+  version '0.9.0'
 
   def install
     (share+'upload-progress-nginx-module').install Dir['*']
